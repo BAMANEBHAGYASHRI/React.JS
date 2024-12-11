bfd9665c3b8150d90200a5c9386069b951a87424
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-12-11 21:53:54 +0530

    create the component and exporting components

---
+++ create-react-component/create-react-comp/src/App.jsx
@@ -0,0 +1,12 @@
+import ReactButton from './reactButton';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ReactButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ create-react-component/create-react-comp/src/reactButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReactButton() { 
+    return <button>Click here</button>;
+}
+
+export default ReactButton;
