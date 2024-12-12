6827235abbd3e64799d065f1a42bc21285efa063
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-12-12 22:06:26 +0530

    Learn how to use Bootstrap in React.js

---
+++ Bootstrap/Learning-Bootstrap/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  return <div>
+    <button type="button" class="btn btn-primary position-relative">
+  Inbox
+  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    99+
+    <span class="visually-hidden">unread messages</span>
+  </span>
+</button>
+  </div>
+  
+}
+
+export default App;
