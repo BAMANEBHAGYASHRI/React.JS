a087797d0a7204c32f2d7e5e08b0cea97a2feab8
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-12-12 21:13:30 +0530

    Dymanic Components

--- create-react-component/create-react-comp/src/App.jsx
+++ create-react-component/create-react-comp/src/App.jsx
@@ -1,3 +1,4 @@
+import Hello from './Hello';
 import ReactButton from './reactButton';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <div>
       <h1>Hello World</h1>
       <ReactButton />
+      < Hello />
     </div>
   );
 }
---
+++ create-react-component/create-react-comp/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello(){
+
+    let myName ='Bhagyashri vijay Bamane';
+    let age = 21;
+
+    return <h1>Hellooo ,  {myName} here!! i am {age} year old.</h1>
+}
+
+export default Hello;
