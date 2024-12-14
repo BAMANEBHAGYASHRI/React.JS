5a0c872e0f418a8034b376fb52e7572bd90bd58f
BAMANEBHAGYASHRI <shrutibamaneb@gmail.com>
2024-12-14 10:52:53 +0530

    Map Method

--- Fragments/Learning-Fragment/src/App.jsx
+++ Fragments/Learning-Fragment/src/App.jsx
@@ -3,19 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 
 function App() {
+  let fruitItems = ['Apple' , 'Banana', 'cheery','DragonnFruit','Papaya'];
   return( 
-  <React.Fragment>
-    <h1>Fruit List</h1>
-  <div class="list-group">
-  <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
-     Fav Fruit List
-  </button>
-  <button type="button" class="list-group-item list-group-item-action">Apple</button>
-  <button type="button" class="list-group-item list-group-item-action">Banana</button>
-  <button type="button" class="list-group-item list-group-item-action">Cheery</button>
-  <button type="button" class="list-group-item list-group-item-action" disabled>Dragon-Fruit</button>
-</div>
-</React.Fragment>
+//   <React.Fragment>
+//     <h1>Fruit List</h1>
+//   <div class="list-group">
+//   <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
+//      Fav Fruit List
+//   </button>
+//   <button type="button" class="list-group-item list-group-item-action">Apple</button>
+//   <button type="button" class="list-group-item list-group-item-action">Banana</button>
+//   <button type="button" class="list-group-item list-group-item-action">Cheery</button>
+//   <button type="button" class="list-group-item list-group-item-action" disabled>Dragon-Fruit</button>
+// </div>
+// </React.Fragment>
+//   );
+
+
+//Map method
+
+    <ul class="list-group">{fruitItems.map((item) => (<li class="list-group-item">{item}</li>) )}
+    </ul>
   );
 }
 
